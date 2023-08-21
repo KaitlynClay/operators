5b5d886f40500348de08400bdb7eb7c5707220f4
Kaitlyn Clay <kclay@students.mchenry.edu>
2023-08-20 20:35:34 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+    var a = 17
+    var b = 5
+
+    var ans1 = a % b
+    println(ans1)
+    var ans2 = a / ans1
+    println(ans2)
+
+    var isCompare = a <= b
+    println(isCompare)
+    var isCompare2 = b == 9
+    println(isCompare2)
+
+    var myAssign = 110
+    myAssign *= 2
+    println(myAssign)
+    myAssign %= 8
+    println(myAssign)
+
+    var myIncrement = a
+    myIncrement ++
+    println(myIncrement)
+    var myDecrement = b --
+    println(myDecrement)
+
+}
